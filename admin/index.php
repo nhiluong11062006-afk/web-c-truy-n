35be307862f2915ae744bb227ec1818854a2a8f4
Nguyen Ngoc Yen Nhi <nguyenngocyennhi181@gmail.com>
2026-04-17 17:20:44 +0700

    a

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,12 @@
 <?php
 session_start();
-// Kết nối database
 include '../connect.php'; 
 
-// Kiểm tra quyền (Sử dụng cột role từ bảng user)
 if (!isset($_SESSION['role']) || $_SESSION['role'] != 1) {
     header("Location: ../login.php");
     exit();
 }
 
-// Lấy module từ URL
 $module = isset($_GET['module']) ? $_GET['module'] : '';
 ?>
 <!DOCTYPE html>
@@ -27,7 +24,6 @@ $module = isset($_GET['module']) ? $_GET['module'] : '';
                     <li><a href="index.php">Trang chủ</a></li>
                     <li><a href="index.php?module=category">Quản lý Loại truyện</a></li>
                     <li><a href="index.php?module=story">Quản lý Truyện</a></li>
-                    <li><a href="index.php?module=chapter">Quản lý Chương</a></li>
                     <li><a href="index.php?module=user">Quản lý User</a></li>
                     <li><a href="../logout.php">Đăng xuất</a></li>
                 </ul>
