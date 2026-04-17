5bab887dce7d86a2babf7a7f55a86a982f35cf9d
Nguyen Ngoc Yen Nhi <nguyenngocyennhi181@gmail.com>
2026-04-17 14:15:01 +0700

    admin

---
+++ admin/modules/user.php
@@ -0,0 +1,23 @@
+<h3>Danh sách thành viên</h3>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Tên đăng nhập</th>
+        <th>Họ tên</th>
+        <th>Quyền</th>
+        <th>Thao tác</th>
+    </tr>
+    <?php
+    $res = mysqli_query($conn, "SELECT * FROM user");
+    while($row = mysqli_fetch_assoc($res)) {
+        $role_text = ($row['role'] == 1) ? "<b>Admin</b>" : "Thành viên";
+        echo "<tr>
+                <td>{$row['user_id']}</td>
+                <td>{$row['username']}</td>
+                <td>{$row['full_name']}</td>
+                <td>$role_text</td>
+                <td><a href='#' class='btn btn-delete'>Khóa tài khoản</a></td>
+              </tr>";
+    }
+    ?>
+</table>
